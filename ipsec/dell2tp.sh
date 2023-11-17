054711c9f0aa1d7f99b76e09c896cded791e9630
Roberto Jiménez <powermx@gmail.com>
2023-11-17 03:32:57 -0600

    Update dell2tp.sh

--- ipsec/dell2tp.sh
+++ ipsec/dell2tp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# SL
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -62,4 +61,3 @@ echo "=========================="
 echo "Username  : $VPN_USER"
 echo "Expired   : $exp"
 echo "=========================="
-echo "Script Mod By SL"
