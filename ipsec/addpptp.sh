8d1d719d3370ae6d5e80184b8bf20c72b612a4b8
Roberto Jiménez <powermx@gmail.com>
2023-11-17 03:32:21 -0600

    Update addpptp.sh

--- ipsec/addpptp.sh
+++ ipsec/addpptp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# SL
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -70,5 +69,4 @@ Password  : $VPN_PASSWORD
 Created   : $hariini
 Expired   : $exp
 ============================
-Script Mod By SL
 EOF
